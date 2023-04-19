b4ec858c6ee1fad91704bd60951c7ecd18e61c76
marco cruz <marco24cruz08@gmail.com>
2023-04-19 14:11:07 -0600

    :seedling: add linter to files

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ const PORT = process.env.PORT || 5000;
 const start = () => {
   try {
     app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
       console.log(`server is running on port: ${PORT}`);
     });
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log(error);
   }
 };

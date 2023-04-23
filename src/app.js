1fb8bd4ecce4d78f8ae5d97ce3659302b7cacd1b
marco cruz <marco24cruz08@gmail.com>
2023-04-23 14:27:05 -0600

    :heavy_plus_sign: add cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import fileRouter from './routes/file.js';
 import userRouter from './routes/user.js';
 import folderRouter from './routes/folder.js';
 
+const corsOptions = {
+  origin: ['http://localhost:4200'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS'],
+};
+
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 const PREFIX = '/api/v1';
 
